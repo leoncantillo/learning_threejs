9cc3c1f70ab6c757c3433998fd630ccfdcf8aebf
Leon Cantillo <leonjob13@gmail.com>
2025-07-22 13:28:55 -0500

    [UPDATE] Learning about lights.

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,23 @@ function App() {
       0.1,
       1000
     );
-    camera.position.z = 5;
+    camera.position.set(0,0,5);
     const renderer = new THREE.WebGLRenderer({canvas: canvasRef.current});
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setClearColor('#111');
 
     const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({color: 0x777});
+    const material = new THREE.MeshStandardMaterial({color: 0x777});
     const cube = new THREE.Mesh(geometry, material);
     cube.rotateX(0.5);
     scene.add(cube);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+    scene.add(ambientLight);
+
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(10, 10, 0);
+    scene.add(light);
 
     const animation = () => {
       cube.rotation.y += 0.01;
