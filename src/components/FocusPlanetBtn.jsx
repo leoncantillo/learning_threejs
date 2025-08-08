9d3515f686a768b7602d40a9db0cdddb73a14e4e
Leon Cantillo <leonjob13@gmail.com>
2025-08-08 15:15:09 -0500

    [UPDATE] Greater modularity.

--- src/components/FocusPlanetBtn.jsx
+++ src/components/FocusPlanetBtn.jsx
@@ -1,40 +1,21 @@
 import '../styles/FocusPlanetBtn.scss';
 
-const FocusPlanetBtn = ({ focusOnPlanet }) => {
+const FocusPlanetBtn = ({ focusOnPlanet, focusedPlanet }) => {
+    const planets = ['sun', 'mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'pluto'];
+
     return (
         <div className='focusPlanetBtns'>
             <h3>Focus A Celestial Body</h3>
             <ul>
-                <li onClick={() => focusOnPlanet('sun')} style={{ margin: '5px' }}>
-                    Look at Sun
-                </li>
-                <li onClick={() => focusOnPlanet('mercury')} style={{ margin: '5px' }}>
-                    Look at Mercury
-                </li>
-                <li onClick={() => focusOnPlanet('venus')} style={{ margin: '5px' }}>
-                    Look at Venus
-                </li>
-                <li onClick={() => focusOnPlanet('earth')} style={{ margin: '5px' }}>
-                    Look at Earth
-                </li>
-                <li onClick={() => focusOnPlanet('mars')} style={{ margin: '5px' }}>
-                    Look at Mars
-                </li>
-                <li onClick={() => focusOnPlanet('jupiter')} style={{ margin: '5px' }}>
-                    Look at Jupiter
-                </li>
-                <li onClick={() => focusOnPlanet('saturn')} style={{ margin: '5px' }}>
-                    Look at Saturn
-                </li>
-                <li onClick={() => focusOnPlanet('uranus')} style={{ margin: '5px' }}>
-                    Look at Uranus
-                </li>
-                <li onClick={() => focusOnPlanet('neptune')} style={{ margin: '5px' }}>
-                    Look at Neptune
-                </li>
-                <li onClick={() => focusOnPlanet('pluto')} style={{ margin: '5px' }}>
-                    Look at Pluto
-                </li>
+                {planets.map(planet => (
+                    <li
+                        key={planet}
+                        onClick={() => focusOnPlanet(planet)}
+                        className={focusedPlanet === planet ? 'focused' : ''}
+                    >
+                            Look at {planet[0].toUpperCase()}${planet.slice(1)}
+                    </li>
+                ))}
             </ul>
         </div>
     );
